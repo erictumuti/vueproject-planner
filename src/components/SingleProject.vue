4bc521b522e182bff675cc872a8d841b4431f0b2
erictumuti <erictumuti114@gmail.com>
2020-12-15 10:38:52 +0300

    handle complete toggle method

--- src/components/SingleProject.vue
+++ src/components/SingleProject.vue
@@ -1,11 +1,11 @@
 <template>
-  <div class="project">
+  <div class="project" :class="{ complete: project.complete }">
     <div class="actions">
 		<h3 @click="showDetails = !showDetails">{{ project.title }}</h3>
 		<div class="icons">
 			<span class="material-icons">edit</span>
 			<span @click="deleteProject" class="material-icons">delete</span>
-			<span class="material-icons">check</span>
+			<span @click="toggleComplete" class="material-icons tick">check</span>
 		</div>
 		</div>
 		<div v-if="showDetails" class="details">
@@ -28,6 +28,15 @@ methods: {
 		fetch(this.uri, {method: 'DELETE'})
 		.then(() => this.$emit('delete', this.project.id))
 		.catch(err => console.log(err))
+	},
+	toggleComplete() {
+		fetch(this.uri, {
+			method: 'PATCH',
+			headers: { 'Content-Type': 'application/json' },
+			body: JSON.stringify({ complete: !this.project.complete })
+			}).then(() => {
+				this.$emit('complete', this.project.id)
+			}).catch(err => console.log(err))
 	}
 }
 }
@@ -40,7 +49,7 @@ methods: {
 	padding: 10px 20px;
 	border-radius: 4px;
 	box-shadow: 1px 2px 3px rgba(0, 0, 0, 0.05);
-	border-left: 4px solid #e90074;
+	border-left: 7px solid #e90074;
 }
 h3 {
 	cursor: pointer;
@@ -58,5 +67,11 @@ h3 {
 }
 .material-icons:hover {
 	color: #777;
+}
+.project.complete {
+	border-left: 7px solid rgb(86, 216, 86);
+}
+.project.complete .tick {
+	color: rgb(86, 216, 86);
 }
 </style>
